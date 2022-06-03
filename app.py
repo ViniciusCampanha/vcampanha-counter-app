e574be9259d7e50c5666cf3f47479246800ac0dd
Vinicius Campanha <vcampanha@orizon.com.br>
2022-06-03 20:55:40 -0300

    terceiro

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import os
+import sys
+
 from flask import Flask
 
 from django.shortcuts import render
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=[ 'GET' ])
 def index():
    return "Hello"
+
+@app.route('/flipacoin', methods=[ 'GET' ])
+def flipacoin():
+    return choice(['Cara', 'Coroa'])
 
 # Create your views here.
 def counterView(request):
@@ -18,3 +25,15 @@ def counterView(request):
    elif request.method == 'POST' and 'reset' in request.POST:
       request.session['count'] = 0
       return render(request,'counter.html')
+
+
+if __name__ == '__main__':
+
+    current_module = os.path.dirname(os.path.curdir)
+    sys.path.append(current_module)
+
+    os.environ['FLASK_APP'] = 'app.py'
+    os.environ['FLASK_ENV'] = 'development'   
+    
+
+    app.run(host='0.0.0.0',port='8080')
